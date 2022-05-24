bb8ab7a73284fa092affb26913403ce2256b5b12
zxspectrummy <93605517+zxspectrummy@users.noreply.github.com>
2022-05-24 05:18:22 +0200

    feat: revert if tie

--- contracts/VoteSystem.sol
+++ contracts/VoteSystem.sol
@@ -11,11 +11,19 @@ contract VoteSystem is Ownable {
     mapping(uint => Ballot) public ballots;
 
     event BallotCreated(string description, string[] candidates, uint32 expirationTime);
-    event VoteCasted();
+    event VoteCasted(uint _ballotId, uint _candidateId);
+    event VoteClosed(uint _ballotId);
+
+    struct CandidateData {
+        string name;
+        address addr;
+    }
 
     struct Candidate {
         string name;
         address addr;
+        uint32 voteCount;
+        bool valid;
     }
 
     struct Ballot {
@@ -23,8 +31,8 @@ contract VoteSystem is Ownable {
         uint32 expirationTime;
         uint candidateCount;
         uint balance;
+        bool isOpen;
         mapping(uint => Candidate) candidates;
-        mapping(uint => uint32) votesCount;
         mapping(address => bool) hasVoted;
     }
 
@@ -32,7 +40,7 @@ contract VoteSystem is Ownable {
         ballotCount = 0;
     }
 
-    function addVoting(string calldata description, Candidate[] calldata candidates) public onlyOwner returns (uint) {
+    function addVoting(string calldata description, CandidateData[] calldata candidates) public onlyOwner returns (uint) {
         Ballot storage newBallot = ballots[ballotCount];
         string[] memory candidateNames = new string[](candidates.length);
         newBallot.description = description;
@@ -40,45 +48,58 @@ contract VoteSystem is Ownable {
         newBallot.candidateCount = candidates.length;
         newBallot.balance = 0;
         for (uint i = 0; i < candidates.length; i++) {
-            newBallot.candidates[i] = candidates[i];
+            newBallot.candidates[i] = Candidate(candidates[i].name, candidates[i].addr, 0, true);
             candidateNames[i] = candidates[i].name;
         }
+        newBallot.isOpen = true;
         emit BallotCreated(newBallot.description, candidateNames, newBallot.expirationTime);
         return ballotCount++;
     }
 
-    function getWinner(uint _ballotId) public view returns (uint winningProposal_) {
-        uint winningVoteCount = 0;
-        for (uint c = 0; c < ballots[_ballotId].candidateCount; c++) {
-            if (ballots[_ballotId].votesCount[c] > winningVoteCount) {
-                winningVoteCount = ballots[_ballotId].votesCount[c];
-                winningProposal_ = c;
-            }
-        }
-    }
-
-    modifier canVote(uint _ballotId) {
-        require(!ballots[_ballotId].hasVoted[msg.sender], "User has already voted");
-        _;
-    }
-
-    modifier isOpen(uint _ballotId) {
-        require(block.timestamp < ballots[_ballotId].expirationTime, "Vote is closed");
-        _;
-    }
-
-    function vote(uint _ballotId, uint _candidateId) external payable canVote(_ballotId) isOpen(_ballotId) {
+    function vote(uint _ballotId, uint _candidateId) external payable {
         require(msg.value == voteFee, "Please provide the voting fee of 0.01 eth");
         Ballot storage ballot = ballots[_ballotId];
+        require(block.timestamp < ballot.expirationTime, "Vote duration has passed");
+        require(!ballot.hasVoted[msg.sender], "User has already voted");
+        require(ballot.candidates[_candidateId].valid == true, "Candidate id does not exist");
         ballot.hasVoted[msg.sender] = true;
-        ballot.votesCount[_candidateId]++;
+        ballot.candidates[_candidateId].voteCount++;
         ballot.balance += voteFee;
+        emit VoteCasted(_ballotId, _candidateId);
     }
 
     function closeVote(uint _ballotId) external {
-        Ballot storage currentBallot = ballots[_ballotId];
-        require(block.timestamp >= ballots[_ballotId].expirationTime, "Vote cannot be closed yet");
-        address payable _winner = payable(currentBallot.candidates[getWinner(_ballotId)].addr);
-        _winner.transfer((currentBallot.balance * 90) / 100);
+        Ballot storage ballot = ballots[_ballotId];
+        require(block.timestamp >= ballot.expirationTime, "Vote duration has not passed yet");
+        require(ballot.balance > 0, "Nobody has voted yet");
+        require(ballot.isOpen == true, "Vote is already closed");
+        address payable winner = payable(_getWinnerCandidateAddress(_ballotId));
+        winner.transfer((ballot.balance * 90) / 100);
+        ballot.isOpen = false;
+        emit VoteClosed(_ballotId);
+    }
+
+    function _getWinnerCandidateAddress(uint _ballotId) private view returns (address) {
+        return ballots[_ballotId].candidates[_getWinnerCandidateId(_ballotId)].addr;
+    }
+
+    function _getWinnerCandidateId(uint _ballotId) private view returns (uint winner_) {
+        uint winningVoteCount = 0;
+        Ballot storage ballot = ballots[_ballotId];
+        for (uint c = 0; c < ballot.candidateCount; c++) {
+            if (ballot.candidates[c].voteCount > winningVoteCount) {
+                winningVoteCount = ballot.candidates[c].voteCount;
+                winner_ = c;
+            }
+        }
+        for (uint x = 0; x < ballot.candidateCount; x++) {
+            for (uint y = x + 1; y < ballot.candidateCount; y++) {
+                if (
+                    winningVoteCount == ballot.candidates[x].voteCount && winningVoteCount == ballot.candidates[y].voteCount
+                ) {
+                    revert("This is a tie vote");
+                }
+            }
+        }
     }
 }
--- test/VoteSystemTest.ts
+++ test/VoteSystemTest.ts
@@ -3,7 +3,7 @@ import { expect } from 'chai';
 import { ethers } from 'hardhat';
 import { VoteSystem } from '../typechain';
 
-describe('Vote System', function () {
+describe('Vote System', () => {
   let voteSystem: VoteSystem;
   let addr1: SignerWithAddress, addr2: SignerWithAddress;
   let addrs: SignerWithAddress[];
@@ -11,6 +11,7 @@ describe('Vote System', function () {
     description: string;
     candidates: { name: string; addr: string }[];
   };
+
   beforeEach(async () => {
     const VoteSystem = await ethers.getContractFactory('VoteSystem');
     voteSystem = await VoteSystem.deploy();
@@ -25,7 +26,7 @@ describe('Vote System', function () {
     };
   });
 
-  it('owner can add voting', async () => {
+  it('should create vote', async () => {
     const tx = await voteSystem.addVoting(ballot.description, ballot.candidates);
     await expect(tx)
       .to.emit(voteSystem, 'BallotCreated')
@@ -41,49 +42,107 @@ describe('Vote System', function () {
     ).to.be.revertedWith('Ownable: caller is not the owner');
   });
 
-  it('users can vote', async () => {
-    await voteSystem.addVoting(ballot.description, ballot.candidates);
-    await expect(voteSystem.vote(0, 0)).to.be.revertedWith(
-      'Please provide the voting fee of 0.01 eth'
-    );
-    await voteSystem.connect(addr1).vote(0, 1, { value: ethers.utils.parseEther('0.01') });
-    await voteSystem.connect(addr2).vote(0, 1, { value: ethers.utils.parseEther('0.01') });
-    expect(await voteSystem.getWinner(0)).to.equal(1);
+  it('should allow users to vote', async () => {
+    await voteSystem
+      .addVoting(ballot.description, ballot.candidates)
+      .then(() =>
+        expect(voteSystem.vote(0, 0)).to.be.revertedWith(
+          'Please provide the voting fee of 0.01 eth'
+        )
+      )
+      .then(() => voteSystem.connect(addr1).vote(0, 1, { value: ethers.utils.parseEther('0.01') }));
   });
 
-  it('user can only vote once', async () => {
-    await voteSystem.addVoting(ballot.description, ballot.candidates);
-    await voteSystem.addVoting('Another vote', ballot.candidates);
-    await voteSystem.connect(addr1).vote(0, 1, { value: ethers.utils.parseEther('0.01') });
-    await expect(
-      voteSystem.connect(addr1).vote(0, 1, { value: ethers.utils.parseEther('0.01') })
-    ).to.be.revertedWith('User has already voted');
-    await voteSystem.connect(addr1).vote(1, 1, { value: ethers.utils.parseEther('0.01') });
+  it('should decline vote if candidate id is invalid', async () => {
+    await voteSystem
+      .addVoting(ballot.description, ballot.candidates)
+      .then(() =>
+        expect(
+          voteSystem.connect(addr1).vote(0, 10, { value: ethers.utils.parseEther('0.01') })
+        ).to.be.revertedWith('Candidate id does not exist')
+      );
   });
 
-  it('user cannot vote if voting expired', async () => {
-    await voteSystem.addVoting(ballot.description, ballot.candidates);
-    await ethers.provider.send('evm_increaseTime', [60 * 60 * 24 * 3]);
-    await expect(
-      voteSystem.connect(addr1).vote(0, 1, { value: ethers.utils.parseEther('0.01') })
-    ).to.be.revertedWith('Vote is closed');
+  it('should allow user to vote only once at the same poll', async () => {
+    await voteSystem
+      .addVoting(ballot.description, ballot.candidates)
+      .then(() => voteSystem.addVoting('Another vote', ballot.candidates))
+      .then(() => voteSystem.connect(addr1).vote(0, 1, { value: ethers.utils.parseEther('0.01') }))
+      .then(() =>
+        expect(
+          voteSystem.connect(addr1).vote(0, 1, { value: ethers.utils.parseEther('0.01') })
+        ).to.be.revertedWith('User has already voted')
+      )
+      .then(
+        () =>
+          expect(voteSystem.connect(addr1).vote(1, 1, { value: ethers.utils.parseEther('0.01') }))
+            .to.not.be.reverted
+      );
+  });
+
+  it('should not allow to vote if voting expired', async () => {
+    await voteSystem
+      .addVoting(ballot.description, ballot.candidates)
+      .then(() => ethers.provider.send('evm_increaseTime', [60 * 60 * 24 * 3]))
+      .then(() =>
+        expect(
+          voteSystem.connect(addr1).vote(0, 1, { value: ethers.utils.parseEther('0.01') })
+        ).to.be.revertedWith('Vote duration has passed')
+      );
   });
 
   it('vote can be closed', async () => {
-    await voteSystem.addVoting(ballot.description, ballot.candidates);
-    await voteSystem.addVoting('another vote', ballot.candidates);
-    await voteSystem.connect(addr2).vote(0, 1, { value: ethers.utils.parseEther('0.01') });
-    await ethers.provider.send('evm_increaseTime', [60 * 60 * 24 * 3]);
-    await expect(await voteSystem.connect(addr1).closeVote(0)).to.changeEtherBalance(
-      addr2,
-      ethers.utils.parseEther('0.009')
-    );
+    await voteSystem
+      .addVoting(ballot.description, ballot.candidates)
+      .then(() => voteSystem.addVoting('another vote', ballot.candidates))
+      .then(() => voteSystem.connect(addr2).vote(0, 1, { value: ethers.utils.parseEther('0.01') }))
+      .then(() => ethers.provider.send('evm_increaseTime', [60 * 60 * 24 * 3]))
+      .then(async () =>
+        expect(await voteSystem.connect(addr1).closeVote(0)).to.changeEtherBalance(
+          addr2,
+          ethers.utils.parseEther('0.009')
+        )
+      );
   });
 
-  it('vote cannot be closed before the expiration time', async () => {
-    await voteSystem.addVoting(ballot.description, ballot.candidates);
-    await expect(voteSystem.connect(addr1).closeVote(0)).to.be.revertedWith(
-      'Vote cannot be closed yet'
-    );
+  it('should not allow closing vote before the expiration time', async () => {
+    await voteSystem
+      .addVoting(ballot.description, ballot.candidates)
+      .then(() =>
+        expect(voteSystem.connect(addr1).closeVote(0)).to.be.revertedWith(
+          'Vote duration has not passed yet'
+        )
+      );
+  });
+
+  it('should not allow closing vote if nobody voted', async () => {
+    await voteSystem
+      .addVoting(ballot.description, ballot.candidates)
+      .then(() => ethers.provider.send('evm_increaseTime', [60 * 60 * 24 * 3]))
+      .then(() =>
+        expect(voteSystem.connect(addr1).closeVote(0)).to.be.revertedWith('Nobody has voted yet')
+      );
+  });
+
+  it('should not allow closing vote cannot if tie', async () => {
+    await voteSystem
+      .addVoting(ballot.description, ballot.candidates)
+      .then(() => voteSystem.connect(addr1).vote(0, 0, { value: ethers.utils.parseEther('0.01') }))
+      .then(() => voteSystem.connect(addr2).vote(0, 1, { value: ethers.utils.parseEther('0.01') }))
+      .then(() => ethers.provider.send('evm_increaseTime', [60 * 60 * 24 * 3]))
+      .then(() =>
+        expect(voteSystem.connect(addr1).closeVote(0)).to.be.revertedWith('This is a tie vote')
+      );
+  });
+
+  it('vote cannot be closed if already closed', async () => {
+    await voteSystem
+      .addVoting(ballot.description, ballot.candidates)
+      .then(() => voteSystem.connect(addr1).vote(0, 0, { value: ethers.utils.parseEther('0.01') }))
+      .then(() => ethers.provider.send('evm_increaseTime', [60 * 60 * 24 * 3]))
+      .then(() => voteSystem.connect(addr1).closeVote(0))
+      .then(() =>
+        expect(voteSystem.connect(addr1).closeVote(0)).to.be.revertedWith('Vote is already closed')
+      );
   });
 });
